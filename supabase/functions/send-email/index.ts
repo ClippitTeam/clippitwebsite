bd9389ae8c736deacbe842eb72af4db4312f237f
Lord Haigen <doesyouremailendin@gmail.com>
2025-11-06 15:06:59 +1000

    Fix CORS: Add Access-Control-Allow-Methods header

--- supabase/functions/send-email/index.ts
+++ supabase/functions/send-email/index.ts
@@ -11,6 +11,7 @@ const SMTP_FROM = 'Clippit <admin@clippit.today>'
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+  'Access-Control-Allow-Methods': 'POST, OPTIONS',
 }
 
 interface EmailRequest {
